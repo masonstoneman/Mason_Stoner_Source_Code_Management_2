a4c3a26838f84b2ca68c5eac9a7d9f19ecb5460c
Mason Stoner <mason.stoner@sinclair.edu>
2020-11-09 00:29:51 -0500

    I changed all of the variable from int and float to double, I fixed the 3rd cin line so that it assigned the to the correct variable and I made some changes to the average function so that it used variables like sum and average and then returned the average variable.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,20 +2,26 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+double average(double i1, double i2, double i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	double sum, average;
+
+	sum = i1 + i2 + i3;
+
+	average = sum / 3;
+
+	return average;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	double n1 =0.0 , n2 = 0.0, n3 = 0.0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
-	float a = average(n1, n2, n3);
+	double a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
